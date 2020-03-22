46a1a56d43226f48c23bbfb89e71fa9451d19a8e
Vlad Holubiev <golubev.vld@gmail.com>
2020-03-22 17:45:03 +0200

    feat: approve PR using API

--- src/features/pull-requests/press-a-to-approve.ts
+++ src/features/pull-requests/press-a-to-approve.ts
@@ -1,20 +1,22 @@
 import delegate from 'delegate-it';
 import select from 'select-dom';
+import domLoaded from 'dom-loaded';
+import OptionsSync from 'webext-options-sync';
+import {Options} from '../../types';
 
-export function enableApprovingPROnAPress(): void {
-  delegate<HTMLElement, KeyboardEvent>('html', 'keypress', (event) => {
+export async function enableApprovingPROnAPress(): Promise<void> {
+  const [org, repo, _, prNumber] = window.location.href.split('/').slice(3);
+
+  delegate<HTMLElement, KeyboardEvent>('html', 'keypress', async (event) => {
     if (!isAKeyPressedInBody(event)) {
-      return;
-    }
-
-    if (!select.exists('#submit-review button[value=approve]')) {
       return;
     }
 
     const wantApprove = confirm('Approve this PR?');
 
     if (wantApprove) {
-      select<HTMLButtonElement>('#submit-review button[value=approve]')?.click();
+      const authHeader = await getAuthHeader();
+      await approvePR({org, repo, prNumber, authHeader});
     }
   });
 }
@@ -26,4 +28,32 @@ function isAKeyPressedInBody(event: KeyboardEvent): boolean {
   console.debug({isKeypressInBody, isKeypressA});
 
   return isKeypressA && isKeypressInBody;
+}
+
+async function getAuthHeader(): string {
+  const optionsStorage = new OptionsSync<Options>();
+  const {ghToken} = await optionsStorage.getAll();
+  console.info('ghToken', ghToken);
+
+  await domLoaded;
+  const username = select<HTMLMetaElement>('meta[name="user-login"]').getAttribute('content');
+
+  return `Basic ${btoa(`${username}:${ghToken}`)}`;
+}
+
+async function approvePR({org, repo, prNumber, authHeader}): void {
+  const resp = await fetch(
+    `https://api.github.com/repos/${org}/${repo}/pulls/${prNumber}/reviews`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: authHeader,
+      },
+      body: JSON.stringify({
+        event: 'APPROVE',
+      }),
+    }
+  );
+
+  return resp.json();
 }
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface Options {
+  ghToken?: string;
+  jiraToken?: string;
+  [key: string]: string | number | boolean;
+}
