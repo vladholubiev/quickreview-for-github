4d263b46c81aacb0a6b5396fa5be60eba0b8991f
Vlad Holubiev <golubev.vld@gmail.com>
2020-03-22 16:30:50 +0200

    feat: implement prs from bots button

--- src/features/notifications/filter-prs-from-bots.tsx
+++ src/features/notifications/filter-prs-from-bots.tsx
@@ -1,12 +1,37 @@
 import domLoaded from 'dom-loaded';
 import React from 'dom-chef';
+import select from 'select-dom';
 
 export async function enableFilteringPRsFromBots(): Promise<void> {
   await domLoaded;
 
-  document.querySelector('.filter-list.js-notification-sidebar-filters').prepend(
-    <li>
-      <a class="filter-item">🤖 PRs from Bots</a>
+  addButton();
+
+  document.addEventListener('pjax:end', () => {
+    addButton();
+  });
+}
+
+function addButton(): void {
+  document.querySelector('.filter-list.js-notification-sidebar-filters')?.prepend(
+    <li onclick={filterPRsFromBots}>
+      <a class="filter-item" id="ghp-prs-from-bots">
+        <span class="count text-normal">{getPRsFromBotsCount()}</span>
+        🤖 PRs from Bots Only
+      </a>
     </li>
   );
+}
+
+function getPRsFromBotsCount(): number {
+  return select.all<HTMLAnchorElement>('a.avatar[href*=bot]').length;
+}
+
+function filterPRsFromBots(): void {
+  select<HTMLAnchorElement>('#ghp-prs-from-bots').classList.add('selected');
+
+  select
+    .all<HTMLAnchorElement>('a.avatar:not([href*=bot])')
+    .map(el => el.closest('.notifications-list-item'))
+    .map(el => el.remove());
 }
