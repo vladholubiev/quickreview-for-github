4e741e35d76a11f1222e325aa2f6ccdcb6a3a1ce
Vlad Holubiev <golubev.vld@gmail.com>
2020-03-26 20:54:14 +0200

    refactor: remove feature to filter out prs

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
-import {enableFilteringPRsFromBots} from './features/notifications/filter-prs-from-bots';
 import {enableViewingFilesOnVPress} from './features/pull-requests/press-v-to-view';
 import {enableApproveMergeShortcuts} from './features/pull-requests/press-a-approve-m-merge';
 
 async function init() {
-  enableFilteringPRsFromBots();
   enableViewingFilesOnVPress();
   enableApproveMergeShortcuts();
 }
