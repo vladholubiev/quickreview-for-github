b90d3dc05d149b7f7b7b198c4d556e3307fd10f8
Vlad Holubiev <golubev.vld@gmail.com>
2020-03-31 11:49:18 +0300

    refactor: don't add webpack reload plugin for prod

--- webpack.config.js
+++ webpack.config.js
@@ -31,13 +31,14 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtensionReloader({
-      manifest: path.resolve(__dirname, 'src/manifest.json')
-    }),
+    process.env.NODE_ENV === 'development' &&
+      new ExtensionReloader({
+        manifest: path.resolve(__dirname, 'src/manifest.json')
+      }),
     new CopyWebpackPlugin([{from: 'src/manifest.json'}]),
     new CopyWebpackPlugin([{from: 'src/options.html'}]),
     new CopyWebpackPlugin([{from: 'src/icons', patterns: ['*.png'], to: 'icons'}])
-  ],
+  ].filter(e => e),
   resolve: {
     modules: [path.resolve(__dirname, 'node_modules')],
     extensions: ['.ts', '.tsx', '.json', '.js']
